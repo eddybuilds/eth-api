a5164ead5158d42a14343f040a03c3e4d2fe6825
eddybuilds <hey@eddybuilds.io>
2022-04-19 13:58:07 -0700

    Get E2E working from Express API to Geth/local node

--- queries.sh
+++
@@ -1,9 +0,0 @@
-# Get Latest Block Hash
-curl --location --request POST 'localhost:8545/' \
---header 'Content-Type: application/json' \
---data-raw '{
-	"jsonrpc":"2.0",
-	"method":"eth_blockNumber",
-	"params":[],
-	"id":83
-}'
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import Web3 from 'web3';
+import express from 'express';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const web3 = new Web3("http://0.0.0.0:8545");
+
+const app = express();
+const port = 3000;
+
+app.get('/version', (req, res) => {
+  res.json({ Result: Web3.version });
+})
+
+app.get('/blockHead', async (req, res, next) => {
+  try {
+    let blockNumber = await web3.eth.getBlockNumber();
+    res.json({ Result: blockNumber });
+  }
+  catch (error) {
+    return next(error);
+  }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
